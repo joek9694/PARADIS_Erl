5aa65c0c2caeaac50ad92e1ae7ddfa2e9ee2c401
Johan <Johan.Eklundh@gmail.com>
2018-01-24 13:39:41 +0100

    finished functions + started testcases and polishing

--- bad_test1.erl
+++ bad_test1.erl
@@ -1,16 +1,43 @@
 -module(bad_test1).
+
+%
+%   This program had a number of errors, which should now be fixed.
+%   Running bad_prog1:test_all() should return the atom 'hooray'
+%
+
 -import(math, [pi/0, pow/2]).
-
-%
-%   This program has a number of errors
-%   find them and fix them.
-%   Once you have fixed the code
-%   run some text examples to see if the fuctions
-%   behave as expected
-%   Running bad_prog1:test() should return the atom 'hooray'
-%
-
 -compile(export_all).
+
+
+
+%% --------- Tests.
+
+test_double() ->
+	4 = double(2),
+	-4 = double(-2),
+	0 = double(0),
+	{'EXIT',_} = (catch double([])),
+	hooray.
+
+%%	io:format("double(2) is ~p, and should be 4 ~n",[double(2)]),
+%%	io:format("double(-2) is ~p, and should be -4 ~n",[double(-2)]),
+%%	io:format("double(0) is ~p, and should be 0 ~n",[double(0)]),
+%%	hooray.
+	
+test_factorial() ->
+	1 = factorial(0),
+	1 = factorial(1),
+	24 = factorial(4),
+	{'EXIT', _Why} = (catch factorial(-1)),
+	{'EXIT', _Why2} = (catch factorial("")).	
+	
+%%	io:format("factorial(0) is ~p, and should be 1 ~n",[factorial(0)]),
+%%	io:format("factorial(1) is ~p, and should be 1 ~n",[factorial(1)]),
+%%	io:format("factorial(4) is ~p, and should be 24 ~n",[factorial(4)]),
+%%	{'EXIT', Why} = (catch factorial(-1)),
+%%	{'EXIT', _Why2} = (catch factorial("")),
+%%	io:format("~p ~n",[Why]),
+%%	hooray.
 
 test_all() ->
     10 = double(5),
@@ -18,18 +45,24 @@ test_all() ->
     44 = perimeter({square,11}),
 	44 = perimeter({rectangle,11,11}),		%added serparate test for tuple with 3 elements
 %%    melting point of sulfur 
-%    {f,212} = temperatuer_convert({c,100}), 
+    {f,212.0} = temperatuer_convert({c,100.0}), 
     120 = factorial(5),
     hooray.
 
-factorial(0) -> 1;
-factorial(N) when N > 0 -> N* factorial(N-1).
-
-test1() ->
-    io:format("double(2) is ~p~n",[double(2)]).
+	
+%% --------- Random mathematical equations.
 	
 double(X) ->
     2*X.
+	
+	
+factorial(0) -> 
+	1;
+factorial(N) when N > 0 -> 
+	N* factorial(N-1).
+
+	
+%% --------- Calculations on geometric figures.
 	
 area({square,X}) ->
     X*X;
@@ -37,23 +70,29 @@ area({rectangle,X,Y}) ->
     X*Y;
 area({circle,R}) ->	
 	 math:pi() * math:pow(R,2).
-	 
-%% temperature conversion 
-%% using formula 5(F-32) = 9C
-
-%%temperature_convert({c,C}) -> 
-%%    F = 9*C/5+32,
-%%    {f,F};
-%%temperature_convert({f,F}) -> 
-%%    C = F-32*5/9,
-%%    {c,C}
-	 
-perimeter(In) ->
-    case In of
+	
+	
+perimeter(InTupple) ->
+    case InTupple of
         {rectangle,X,Y} ->
              2*(X+Y);
 		{square,X} ->
              4*X;
         _ ->
-            io:format("cannot compute the area of ~p~n",In)
-			end.
+            io:format("cannot compute the area of ~p~n",InTupple)
+	end.
+
+	
+%% ---------- temperature conversion.
+%%(without guards for below absolute zero conversions,
+%% since there (since 2013) are quantum gas with measured 
+%% sub-absolute-zero temperatures.
+%% using formula 5(F-32) /9 = C
+
+temperatuer_convert({c,C}) ->
+	F = (9*C) /5 + 32,
+	{f,F};
+
+temperatuer_convert({f,F}) ->
+	C = 5 *(F -32) /9,
+	{c,C}.
