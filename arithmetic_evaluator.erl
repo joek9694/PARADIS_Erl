364ea860659dbdd62e911ceade55c3268a2a32fe
joek9694 <johan.eklundh@gmail.com>
2018-01-30 23:34:12 +0100

    arithmetic_evaluator 1.0

--- arithmetic_evaluator.erl
+++ arithmetic_evaluator.erl
@@ -2,3 +2,26 @@
 
 -compile(export_all).
 
+
+eval(X) when is_number(X) ->
+	X;
+	 
+eval({plus, A, B}) when is_number(A),is_number(B)->
+	A + B;
+eval({minus, A, B}) when is_number(A),is_number(B)->
+	A - B;
+eval({times, A, B}) when is_number(A),is_number(B)->
+	A * B;
+eval({divide, A, B}) when is_number(A),is_number(B)->
+	A / B;
+	
+eval(E) when is_tuple(E) , tuple_size(E) > 2 -> 
+	eval(lists:reverse(tuple_to_list(E)));
+	
+eval([H|T]) when is_atom(H) ->
+	eval(list_to_tuple([H|T]));
+	
+eval(E) when is_list(E) ->
+	[H|T] = E,
+	eval(T ++ [eval(H)]).
+	
