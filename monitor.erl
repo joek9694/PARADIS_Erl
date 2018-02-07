b9d69a043cd169b8ef6ab288713ad6f05a9b392c
joek9694 <johan.eklundh@gmail.com>
2018-02-07 23:47:10 +0100

    monitor 0.2

--- monitor.erl
+++ monitor.erl
@@ -4,17 +4,17 @@
 
 start() -> 
     MonitorPid = spawn(fun() -> monitor() end),
-    io:format("THE MONITORPID: ~p~n", [MonitorPid]).
+    io:format("THE MONITORPID: ~p~n", [MonitorPid]).        %% Clean up
 
 monitor() ->
-    double:start(),     %% double
+    double:start(),     %% process registered as 'double'
     monitor(process, double),
     loop().
 
 loop()  ->
     receive 
         {'DOWN', Ref, process, Pid, Info} ->
-            io:format("Error: ~p~n", [Info]),
+            io:format("Error: ~p~n", [Info]),               %% Clean up
             double:start(),
             monitor(process, double),
             loop()
